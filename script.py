aaaed94f7834487435300c23d073c4fce5c05da3
SamtiagoSR <samtiago.santa.ramirez@gmail.com>
2021-05-29 21:41:48 -0500

    Script py python3

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import os 
 # os.system('julia installpkg.jl')
-os.system('python SOR.py')
+os.system('python3 SOR.py')
 os.system('julia sor2.jl')
 os.system('scalac SOR.scala')
 os.system('scala SOR')
